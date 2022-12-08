77d3eb88bd1f54845423b306b7c731d79c45b323
César Garcia (希 賽) <cesar.garcia.mazariegos@outlook.com>
2022-12-08 12:15:34 +0800

    infraestructure deleted

--- app/infrastructure/youtube_api/youtube_constants.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module UFeeling
-  REGIONS = { TAIWAN: 'TW', MEXICO: 'MX', GUATEMALA: 'GT', NICARAGUA: 'NI' }.freeze
-  PATH_FILTERS = { SNIPPET: 'snippet', STATISTICS: 'statistics', CONTENT_DETAILS: 'contentDetails',
-                   REPLIES: 'replies' }.freeze
-  CHART_FILTERS = { MOST_POPULAR: 'mostPopular' }.freeze
-end
