5f800fee2f27ab15c54b036407c23605a4eb987b
César Garcia (希 賽) <cesar.garcia.mazariegos@outlook.com>
2023-01-07 23:57:01 +0800

    shorts

--- app/application/services/add_video.rb
+++ app/application/services/add_video.rb
@@ -16,8 +16,8 @@ module UFeeling
 
       def parse_url(input)
         if input.success?
-          from_param_fild = input[:video_url].include?('https://youtu.be/') || input[:video_url].include?('https://youtube.com/shorts/')
-          video_id = from_param_fild ? video_id_from_param_fild(input[:video_url]) : video_id_query_field(input[:video_url])
+          from_param_fild = input[:video_url].include?('youtu.be/') || input[:video_url].include?('youtube.com/shorts/')
+          video_id = from_param_fild ? id_from_param_fild(input[:video_url]) : id_query_field(input[:video_url])
           Success(video_id:)
         else
           Failure("URL #{input.errors.messages.first}")
@@ -43,11 +43,11 @@ module UFeeling
         Failure('Error in the video -- please try again')
       end
 
-      def video_id_from_param_fild(video_url)
+      def id_from_param_fild(video_url)
         video_url.split('?')[0].split('/')[-1]
       end
 
-      def video_id_query_field(video_url)
+      def id_query_field(video_url)
         includes_other_params = video_url.include? '&'
         includes_other_params ? video_url.split('=')[-2] : video_url.split('=')[-1]
       end
